85564346056aff88af4a568b23311685d554698b
“Alex <alexandriadelaney923@gmail.com>
2021-10-12 14:37:04 -0700

    made it so that once a nav link is clicked the menu icon collapses

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $('.project-image').hover(function () {
     $(this).siblings().toggleClass('hide')
 })
+$('.navbar-nav').on('click', function () {
+    $('.navbar-toggler').toggleClass('collapsed')
+    $('.navbar-collapse').toggleClass('show')
+})
