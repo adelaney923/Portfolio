b9e0a3518439a8dac5b70e8239240c2ab5a9b59d
“Alex <alexandriadelaney923@gmail.com>
2021-10-12 17:15:19 -0700

    added function for google sheets form api. not working, need to debug.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//contact form api
+const subForm = (event) => {
+    event.preventDefault()
+  $.ajax({
+    url: "https://api.apispreadsheets.com/data/19335/",
+    type: "post",
+    data: $("#contact-form").serializeArray(),
+    success: function () {
+      console.log("linked");
+      alert("Form Data Submitted :)");
+    },
+    error: function () {
+      alert("There was an error :(");
+    },
+  });
+}
+
+$('#form-button').on('submit', subForm)
+
+
+
+
+//jquery animations
 $('.project-image').hover(function () {
     $(this).siblings().toggleClass('hide')
 })
